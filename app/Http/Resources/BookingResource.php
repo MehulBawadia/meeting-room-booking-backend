bb3ddd86523c3105a18339578170b97a76f38ed8
Mehul Bawadia <mehulbawadia@gmail.com>
2025-06-04 20:00:50 +0530

    Fix: Change in Booking Resource
    Changed the format of start_time and end_time.
    Added the duration in minutes.

--- app/Http/Resources/BookingResource.php
+++ app/Http/Resources/BookingResource.php
@@ -19,9 +19,10 @@ class BookingResource extends JsonResource
             'user_id' => $this->user_id,
             'meeting_room_id' => $this->meeting_room_id,
             'name_of_meeting' => $this->name_of_meeting,
-            'start_time' => $this->start_time->toDateTimeString(),
-            'end_time' => $this->end_time->toDateTimeString(),
+            'start_time' => $this->start_time->format('d-M-Y h:i a'),
+            'end_time' => $this->end_time->format('d-M-Y h:i a'),
             'number_of_members' => $this->number_of_members,
+            'duration' => $this->start_time->diffInMinutes($this->end_time),
 
             'user' => new UserResource($this->whenLoaded('user')),
             'meeting_room' => new MeetingRoomResource($this->whenLoaded('meetingRoom')),
